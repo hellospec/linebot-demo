1efc799e6247f8e7e7e446d2fbe893df65013aee
preecha <preechac@hellospec.com>
2024-02-03 23:24:02 +0700

    Able to reply LINE Bot message
    - add lienbot_controller.rb for call back from LINE
    - add reply_linebot.rb to process the message and reply
    - add test for reply_linebot.rb

---
+++ app/models/concerns/reply_linebot.rb
@@ -0,0 +1,31 @@
+module ReplyLinebot
+  def match_message(message)
+    message.downcase.match(/^ppx\s+(\d{1,})\b/)
+  end
+
+  def handle_message(message)
+    result = match_message(message)
+    if result.present?
+      reply_message("got x #{result[1]}")
+    end
+  end
+
+  def reply_message(message)
+    access_token = ENV["LINE_MSG_CHANNEL_TOKEN"]
+    conn = Faraday.new(
+      url: 'https://api.line.me/v2/bot/message/reply',
+      headers: {"Content-Type": 'application/json', Authorization: "Bearer #{access_token}"}
+    )
+    response = conn.post do |req|
+      req.body = {
+        replyToken: params["events"][0]["replyToken"],
+        messages: [
+          {
+            type: "text",
+            text: "#{message}"
+          }
+        ]
+      }.to_json
+    end
+  end
+end
