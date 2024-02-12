1cf72d6694866e5d2b09fb11157cc4493a277a1c
woot <worrawutp@gmail.com>
2024-02-12 14:09:49 +0700

    Fix format according to standardrb

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -2,11 +2,11 @@ class LinebotController < ApplicationController
   include ReplyLinebot
   skip_before_action :verify_authenticity_token
 
-	def create
-    unless params["events"][0]["type"] == "message" && params["events"][0]["message"]["type"] == "text" 
-      return 
+  def create
+    unless params["events"][0]["type"] == "message" && params["events"][0]["message"]["type"] == "text"
+      return
     end
     message = params["events"][0]["message"]["text"]
     handle_message(message)
-	end
+  end
 end
