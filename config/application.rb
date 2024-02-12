89a6e66e173c3a1abf064a12a12dc065cb64c751
woot <worrawutp@gmail.com>
2024-02-12 14:05:32 +0700

    Add standardrb to the project

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module LinebotDemo
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w[assets tasks])
     config.hosts << /.*ngrok-free\.app/
 
     # Configuration for the application, engines, and railties goes here.
