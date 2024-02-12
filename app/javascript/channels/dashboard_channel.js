f9c857deb3aad5f0fa56c44ea39bea51ff9cf6d7
woot <worrawutp@gmail.com>
2024-02-12 17:45:08 +0700

    Only subscribe to channel for dashboard page

--- app/javascript/channels/dashboard_channel.js
+++ app/javascript/channels/dashboard_channel.js
@@ -1,24 +1,28 @@
 import consumer from "channels/consumer"
 
-consumer.subscriptions.create({channel: "DashboardChannel", room: "line_chatbot"}, {
-  initialized() {
-  },
-  connected() {
-  },
-  disconnected() {
-  },
-  received(data) {
-    let v = parseInt(data.value)
-    this.updateDashboardValue(v)
-  },
+let dashboard = document.getElementById("dashboard")
+if(dashboard != null) {
+  console.log(dashboard)
+  consumer.subscriptions.create({channel: "DashboardChannel", room: "line_chatbot"}, {
+    initialized() {
+    },
+    connected() {
+    },
+    disconnected() {
+    },
+    received(data) {
+      let v = parseInt(data.value)
+      this.updateDashboardValue(v)
+    },
 
-  updateDashboardValue(value) {
-    let target = document.getElementById("dashboard-value")
-    if(target != null) {
-      let currentValue = parseInt(target.dataset.value)
-      let newValue = currentValue + value
-      target.textContent = newValue
-      target.dataset.value = newValue
+    updateDashboardValue(value) {
+      let target = document.getElementById("dashboard-value")
+      if(target != null) {
+        let currentValue = parseInt(target.dataset.value)
+        let newValue = currentValue + value
+        target.textContent = newValue
+        target.dataset.value = newValue
+      }
     }
-  }
-})
+  })
+}
