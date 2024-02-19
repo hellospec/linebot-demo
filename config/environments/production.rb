4fc7a8e02fc0921593be9f2cda87329b3a995e4b
woot <worrawutp@gmail.com>
2024-02-19 21:55:33 +0700

    Able to start app with docker-compose up
    - Add docker-compose file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,8 @@ Rails.application.configure do
   # config.assume_ssl = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  # config.force_ssl = true
+  config.force_ssl = false
 
   # Log to STDOUT by default
   config.logger = ActiveSupport::Logger.new($stdout)
@@ -94,4 +95,7 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.hosts = [
+    "localhost"
+  ]
 end
