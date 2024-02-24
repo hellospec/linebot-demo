9187158ddbd5be8af653f04149ab1e7550a9a301
woot <worrawutp@gmail.com>
2024-02-24 22:40:42 +0700

    Can also edit the product

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -2,6 +2,10 @@ class Admin::ProductsController < ApplicationController
   def new
   end
 
+  def edit
+    @product = Product.find params[:id]
+  end
+
   def create
     product = Product.new product_params
     if product.save
@@ -12,6 +16,21 @@ class Admin::ProductsController < ApplicationController
           "error-message",
           partial: "admin/error_message",
           locals: {object: product}
+        )
+      ]
+    end
+  end
+
+  def update
+    @product = Product.find params[:id]
+    if @product.update(product_params)
+      redirect_to admin_path, status: :see_other
+    else
+      render turbo_stream: [
+        turbo_stream.replace(
+          "error-message",
+          partial: "admin/error_message",
+          locals: {object: @product}
         )
       ]
     end
