99f2d8671ab4ec1fad7f95b3ca5f4df1dcf0e2ea
woot <worrawutp@gmail.com>
2024-02-27 19:17:23 +0700

    Able to handle api request and create Sale record

--- app/controllers/api/sale_orders_controller.rb
+++ app/controllers/api/sale_orders_controller.rb
@@ -4,6 +4,27 @@ class Api::SaleOrdersController < ApplicationController
   before_action :authenticate_api!
 
   def create
-    render json: {message: "good job"}
+    capture = ApiMessageCapture.new(current_user, sale_order_params)
+    if capture.valid?
+      sale = capture.create_sale!
+      capture.broadcast_to_dashboard
+
+      render json: sale
+    else
+      render json: {error: capture.error}, status: :unprocessable_entity
+    end
+
+  rescue => e
+    render json: {error: e.to_s}, status: :bad_request and return
+  end
+
+  private
+
+  def sale_order_params
+    params.require(:sale).permit(
+      :product_code,
+      :channel_code,
+      :amount
+    )
   end
 end
