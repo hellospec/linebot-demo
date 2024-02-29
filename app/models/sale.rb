0c6baa8cd60431edc1d23cad779d0803cf80c700
woot <worrawutp@gmail.com>
2024-02-29 20:31:16 +0700

    Add class method sum_amount_of to Sale
    - This method return list of object that we will use as data source in
      echart

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,16 +1,27 @@
 class Sale < ApplicationRecord
   belongs_to :user
 
+  validates :amount, numericality: { only_integer: true, greater_than: 0 }
+  validates :product_code, presence: true
+  validates :channel_code, presence: true
+
   scope :total_amount, -> { sum(:amount) }
   scope :dashboard_data, -> {
     {
       total_amount: total_amount,
-      amount_by_product: Sale.group(:product_code).sum(:amount),
-      amount_by_channel: Sale.group(:channel_code).sum(:amount)
+      amount_by_product: sum_amount_of(:product_code),
+      amount_by_channel: sum_amount_of(:channel_code)
     }
   }
 
-  validates :amount, numericality: { only_integer: true, greater_than: 0 }
-  validates :product_code, presence: true
-  validates :channel_code, presence: true
+  class << self
+    def sum_amount_of(name)
+      raise "Not recognize name: #{name}" unless Sale.column_names.include? name.to_s
+
+      data = Sale.group(name.to_sym).sum(:amount)
+      data.map do |k,v|
+        {product: k, amount: v}
+      end
+    end
+  end
 end
