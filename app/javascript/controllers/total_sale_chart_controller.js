7e13fa2a3d25db59864647115fbe9bc47b550be0
woot <worrawutp@gmail.com>
2024-03-03 13:05:08 +0700

    Able to updage amount value to chart table according to broadcast

--- app/javascript/controllers/total_sale_chart_controller.js
+++ app/javascript/controllers/total_sale_chart_controller.js
@@ -4,7 +4,7 @@ import * as echarts from "echarts"
 import {initTotalSaleChart, totalSaleChartOption} from "../src/charts/total_sale_chart"
 
 export default class TotalSaleChartController extends Controller {
-  static targets = ["chart", "totalAmount"]
+  static targets = ["chart", "totalAmount", "dataTable"]
   static values = { 
     data: Array 
   }
@@ -15,11 +15,24 @@ export default class TotalSaleChartController extends Controller {
   // Values callback
   dataValueChanged(data, previousData) {
     this.renderChart()
+    this.renderDataTable()
   }
 
   renderChart() {
     let chartOption = totalSaleChartOption(this.dataValue)
     let pie = initTotalSaleChart("total-sale-chart")
     pie.setOption(chartOption)
+  }
+
+  renderDataTable() {
+    this.dataValue.forEach(d => {
+      let valueElement = this.dataTableTarget.querySelector(`[data-product-name="${d.product}"]`)
+      valueElement.textContent = d.amount.toLocaleString()
+    })
+    this.totalAmountTarget.textContent = this.totalAmount().toLocaleString()
+  }
+
+  totalAmount() {
+    return this.dataValue.reduce((acc, d) => acc + d.amount, 0)
   }
 }
